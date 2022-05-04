9b69a26cd2b751cf13c8cc06c1c47c2933be8a8f
Abhishek Gupta <87366992+agagupta92@users.noreply.github.com>
2022-05-04 12:02:21 +0530

    Time: 5 ms (84.24%), Space: 45.2 MB (57.78%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+        int num = 0;
+        int l = s.length();
+        int last = 1000;
+        for (int i = 0; i < l; i++){
+            int v = getValue(s.charAt(i));
+            if (v > last) num = num - last * 2;
+            num = num + v;
+            last = v;
+        }
+        return num;
+    }
+    private int getValue(char c){
+        switch(c){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
+        }
+    }
+}
