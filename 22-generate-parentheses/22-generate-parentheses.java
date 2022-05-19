83c987410ac67133b045e2a9322df6553f6be0a2
Abhishek Gupta <87366992+agagupta92@users.noreply.github.com>
2022-05-19 23:43:26 +0530

    Time: 3 ms (37.86%), Space: 44.2 MB (21.21%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         
-        ArrayList<String> result = new ArrayList<String>();
+      /*  ArrayList<String> result = new ArrayList<String>();
   ArrayList<Integer> diff = new ArrayList<Integer>();
   result.add("");
   diff.add(0);
@@ -23,7 +23,25 @@ class Solution {
     result = new ArrayList<String>(temp1);
     diff = new ArrayList<Integer>(temp2);
   }
-  return result;
+  return result;*/
+        
+         List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+    
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+        
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    
 
     }
 }
