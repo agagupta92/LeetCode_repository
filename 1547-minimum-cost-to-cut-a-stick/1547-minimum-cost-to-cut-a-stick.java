917b882c1b0dd5a8d6e4a0e91b2f463a5f8160dc
Abhishek Gupta <87366992+agagupta92@users.noreply.github.com>
2022-06-10 00:07:55 +0530

    Time: 15 ms (88.64%), Space: 43.1 MB (58.85%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,4 +1,5 @@
 class Solution {
+    //Tabulation
     public int minCost(int n, int[] cuts) {
         int [] temp = new int[cuts.length+2];
         temp[0] = 0;
@@ -10,11 +11,24 @@ class Solution {
         temp[m]=n;
         int c = cuts.length;
         Arrays.sort(temp);
-        int[][] dp = new int[c+1][c+1];
+        int[][] dp = new int[c+2][c+2];
         for(int []d:dp){
-            Arrays.fill(d,-1);
+            Arrays.fill(d,0);
         }
-       return func(1,c,temp,dp);
+        
+          for(int i=c;i>=1;i--){
+                    for(int j=1;j<=c;j++){
+                        if(i>j) continue;
+                        int mini = Integer.MAX_VALUE;
+                        for(int ind =i;ind<=j;ind++){
+                            int cost = temp[j+1]-temp[i-1]+
+                                dp[i][ind-1]+dp[ind+1][j];
+                            mini = Math.min(mini,cost);
+                        }
+                        dp[i][j]=mini;
+                    }
+                }
+                return dp[1][c];
     }
     
     public int func(int i ,int j,int[]cuts,int[][] dp){
@@ -28,37 +42,3 @@ class Solution {
                 return dp[i][j] = mini;
     }
 }
-
-
-
-/*
- int [] temp = new int[cuts.length+2];
-        int c = cuts.length;
-        temp[0] = 0;
-        int m =1;
-        for(int i =0;i<cuts.length;i++){
-            temp[m]=cuts[i];
-            m++;
-        }
-        temp[m]=n;
-        Arrays.sort(temp);
-        System.out.print(temp.length);
-         System.out.println(n);
-         System.out.println(c);
-        int[][] dp = new int[c+2][c+2];
-        for(int []d:dp){
-            Arrays.fill(d,0);
-        }
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=n;j++){
-                if(i>j) continue;
-                int mini = Integer.MAX_VALUE;
-                for(int ind =i;ind<=j;ind++){
-                    int cost = cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
-                    mini = Math.min(mini,cost);
-                }
-                dp[i][j]=mini;
-            }
-        }
-        return dp[1][c];
-        */
