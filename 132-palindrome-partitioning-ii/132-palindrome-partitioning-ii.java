63d0137bd8363911371f124b3339f692a36c579c
Abhishek Gupta <87366992+agagupta92@users.noreply.github.com>
2022-06-11 17:07:33 +0530

    Time: 2498 ms (5.01%), Space: 41.6 MB (90.51%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        return func(0,n,s,dp)-1;
+    }
+    public int func(int i,int n ,String s,int[]dp){
+        if(i ==n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int mincost = Integer.MAX_VALUE;
+        for(int j =i;j<n;j++){
+            if(isPalindrome(s,i,j)){
+                int cost = 1 + func(j+1,n,s,dp);
+                mincost = Math.min(mincost,cost);
+            }
+        }
+        return dp[i] = mincost; 
+    }
+    boolean isPalindrome(String s,int start,int end){
+        while(start <= end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;end--;  
+        }
+        return true;
+    }
+}
